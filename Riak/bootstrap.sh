dd14f859b6a9023305e1b65adc779692c31efe37
James Kirk <james.kirk84@gmail.com>
2015-03-09 16:20:48 +0000

    riak provisioning

--- Riak/bootstrap.sh
+++ Riak/bootstrap.sh
@@ -1,3 +1,20 @@
-apt-get update
-curl https://packagecloud.io/install/repositories/basho/riak/script.deb | sudo bash
-apt-get install riak=2.0.5-1
+#!/usr/bin/env bash
+
+main(){
+  apt-get update
+  install_deps
+  install_riak
+}
+
+install_deps(){
+  apt-get install -y build-essential libc6-dev-i386 git g++ libncurses5-dev openssl libssl-dev fop xsltproc unixodbc-dev erlang libpam0g-dev
+}
+
+install_riak(){
+  wget http://s3.amazonaws.com/downloads.basho.com/riak/2.0/2.0.5/riak-2.0.5.tar.gz
+  tar zxf riak-2.0.5.tar.gz
+  cd riak-2.0.5
+  make rel
+}
+
+main
